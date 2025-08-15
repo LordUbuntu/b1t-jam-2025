f2fd5df89a654dfdb5a2ee2aca4f573fc6e0d51e
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-15 00:29:18 -0700

    initialize sprites

--- gallant bloom/src/main.js
+++ gallant bloom/src/main.js
@@ -1,11 +1,12 @@
 import kaplay from "kaplay";
-// import "kaplay/global"; // uncomment if you want to use without the k. prefix
+import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+kaplay();
 
-k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
-
-k.onClick(() => k.addKaboom(k.mousePos()));
+// resources
+loadRoot("./"); // A good idea for Itch.io publishing later
+loadSprite("player", "sprites/bean.png");
+loadSprite("bridge", "sprites/bridge_tmp.png");
+loadSprite("elevator", "sprites/elevator_tmp.png");
+loadSprite("ground", "sprites/ground_tmp.png");
