19026605e305b33f6ffdc17626d1ae1c4231ea0e
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-15 00:30:07 -0700

    add first level

--- gallant bloom/src/main.js
+++ gallant bloom/src/main.js
@@ -10,3 +10,48 @@ loadSprite("player", "sprites/bean.png");
 loadSprite("bridge", "sprites/bridge_tmp.png");
 loadSprite("elevator", "sprites/elevator_tmp.png");
 loadSprite("ground", "sprites/ground_tmp.png");
+
+let player = null;
+
+
+// levels
+addLevel([
+  "##########",
+  "#        #",
+  "#        #",
+  "#    #####",
+  "#  @^    #",
+  "#  ###   #",
+  "#     ^  #",
+  "#     ## #",
+  "#    ^   #",
+  "##########",
+], {
+    tileWidth: 64,
+    tileHeight: 64,
+    tiles: {
+      "#": () => [
+        sprite("ground"),
+        area(),
+        body({isStatic: true}),
+        "ground",
+      ],
+      "^": () => [
+        sprite("elevator"),
+        body({isStatic: true}),
+        "elevator",
+      ],
+      "@": () => {
+        const player_components = [
+          sprite("player"),
+          area(),
+          body(),
+          "player",
+        ];
+        onAdd("player", (object) => {
+          if (!player) player = object;
+        });
+        return player_components;
+      },
+    },
+})
